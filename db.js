bd51c8357d63ea1ff3af5ca138a8389d0caa0826
Andy Klimczak <andyklimczak@fastmail.com>
2016-04-04 21:15:51 -0400

    cleaner mongo connection message

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ const mongo_uri = process.env.MONGOLAB_URI || 'mongodb://localhost/report';
 
 mongoose.connect(mongo_uri, (err, res) => {
   if(err) {
-    console.log('error', err);
+    console.log('Error connecting to mongo', err);
   } else {
-    console.log('mongo connect success');
+    console.log('Successfully connected to mongo');
   }
 });
