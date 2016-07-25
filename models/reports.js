86acfbd2a546a806e7b565b27a4a10d7581d5889
Andy Klimczak <andyklimczak@fastmail.com>
2016-07-25 19:49:18 -0400

    Remove POST from router actions

--- models/reports.js
+++ models/reports.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const reportSchema = new mongoose.Schema({
   product: String,
--- routes/reports.js
+++ routes/reports.js
@@ -11,22 +11,4 @@ module.exports = app => {
       }
     });
   })
-  .post((req, res) => {
-    new Report({
-      product: req.body.product,
-      manufacturer: req.body.manufacturer,
-      category: req.body.category,
-      videoTitle: req.body.videoTitle,
-      videoCode: req.body.videoCode,
-      dateReleased: req.body.dateReleased,
-      rating: req.body.rating,
-      effectiveness: req.body.effectiveness
-    }).save((err, report) => {
-      if(err) {
-        res.status(412).json({msg: err.message});
-      } else {
-        res.json(report);
-      }
-    });
-  });
 };
