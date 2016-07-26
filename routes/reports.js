dc50f83d16dbbb9f5a34b12e4521f0cdbe71e5a6
Andy Klimczak <andyklimczak@fastmail.com>
2016-07-25 20:03:44 -0400

    simplify /reports route

--- routes/reports.js
+++ routes/reports.js
@@ -2,13 +2,11 @@ const Report = require('../models/reports.js');
 
 module.exports = app => {
   app.route('/reports')
-  .get((req, res) => {
-    Report.find({}, (err, reports) => {
-      if(err) {
-        res.status(412).json({msg: error.message});
-      } else {
-        res.json(reports);
-      }
-    });
-  })
+    .get((req, res) => {
+      Report.findAll({})
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
 };
