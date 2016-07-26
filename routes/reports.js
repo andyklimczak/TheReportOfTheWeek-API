ee7a999b551dfb9e2b66d4cc4f14ac30867cf327
Andy Klimczak <andyklimczak@fastmail.com>
2016-07-25 20:09:58 -0400

    Revert "simplify /reports route"
    This reverts commit dc50f83d16dbbb9f5a34b12e4521f0cdbe71e5a6.

--- routes/reports.js
+++ routes/reports.js
@@ -2,11 +2,13 @@ const Report = require('../models/reports.js');
 
 module.exports = app => {
   app.route('/reports')
-    .get((req, res) => {
-      Report.findAll({})
-        .then(result => res.json(result))
-        .catch(error => {
-          res.status(412).json({msg: error.message});
-        });
-    })
+  .get((req, res) => {
+    Report.find({}, (err, reports) => {
+      if(err) {
+        res.status(412).json({msg: error.message});
+      } else {
+        res.json(reports);
+      }
+    });
+  })
 };
