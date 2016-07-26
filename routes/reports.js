fc71ef42ac601509371c0f7a30ca27da86c3e286
Andy Klimczak <andyklimczak@fastmail.com>
2016-07-25 20:11:48 -0400

    Sort by newest

--- routes/reports.js
+++ routes/reports.js
@@ -3,7 +3,7 @@ const Report = require('../models/reports.js');
 module.exports = app => {
   app.route('/reports')
   .get((req, res) => {
-    Report.find({}, (err, reports) => {
+    Report.find({}).sort('-dateReleased').exec((err, reports) => {
       if(err) {
         res.status(412).json({msg: error.message});
       } else {
