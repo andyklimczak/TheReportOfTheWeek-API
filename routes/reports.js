b2fd0e1b13b8afb35afde5392d7585dfe76ee948
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-05 18:25:16 -0500

    add query param filter

--- routes/reports.js
+++ routes/reports.js
@@ -1,10 +1,12 @@
 const Report = require('../models/reports.js');
+const MongoQS = require('mongo-querystring');
 
 module.exports = app => {
   app.route('/reports')
   .get((req, res) => {
-    Report.find({}).sort('dateReleased').exec((err, reports) => {
-      if(err) {
+    const qs = new MongoQS({});
+    Report.find(qs.parse(req.query)).sort('dateReleased').exec((error, reports) => {
+      if(error) {
         res.status(412).json({msg: error.message});
       } else {
         res.json(reports);
