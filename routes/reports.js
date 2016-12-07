54b5007be5cce84b53e4d22cf6979f44c7741e1f
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-06 23:05:39 -0500

    Add between filter

--- routes/reports.js
+++ routes/reports.js
@@ -4,7 +4,11 @@ const MongoQS = require('mongo-querystring');
 module.exports = app => {
   app.route('/reports')
   .get((req, res) => {
-    const qs = new MongoQS({});
+    const qs = new MongoQS({
+      custom: {
+        between: 'dateReleased'
+      }
+    });
     Report.find(qs.parse(req.query)).sort('dateReleased').exec((error, reports) => {
       if(error) {
         res.status(412).json({msg: error.message});
